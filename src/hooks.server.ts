d4e0805b7a4fcb478d61f249309ce3f4dedb3ab5
maia <iam@maia.lgbt>
2025-03-04 17:47:50 +0100

    added filesize handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
 import { validateAndGetSession } from "$lib/auth";
-import type { Handle } from "@sveltejs/kit";
+import { error, type Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handleAuth: Handle = async ({ event, resolve }) => {
     // Validate session cookie
     const session = event.cookies.get('session') as string;
 
@@ -17,3 +18,30 @@ export const handle: Handle = async ({ event, resolve }) => {
     const response = await resolve(event);
     return response;
 };
+
+export const handleMaxSize: Handle = async ({ event, resolve }) => {
+    const defaultMaxBytesSize = 512 * 1000 // 512KB
+    const maxUploadFileSize = 10 * 1000 * 1000 // 10MB
+
+    const contentLength = +(event.request.headers.get('content-length') ?? 0);
+
+    if (event.url.pathname === '/feed?/post') {
+        if (contentLength > maxUploadFileSize) {
+            return error(413, {
+                message: 'Request entity too large'
+            });
+        } else {
+            return resolve(event);
+        }
+    }
+
+    if (contentLength > defaultMaxBytesSize) {
+        return error(413, {
+            message: 'Request entity too large'
+        });
+    }
+
+    return resolve(event);
+}
+
+export const handle = sequence(handleAuth, handleMaxSize);
